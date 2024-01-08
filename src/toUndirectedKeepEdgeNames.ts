c2c3f75a88d48c26d78b5ac2cb1ab61867fa5a4d
sn <86227911+tbui17@users.noreply.github.com>
2024-01-07 22:41:18 -0700

    change impl to lib patch

--- src/toUndirectedKeepEdgeNames.ts
+++ src/toUndirectedKeepEdgeNames.ts
@@ -1,6 +1,6 @@
 import type Graph from "graphology"
-import { toUndirected } from "graphology-operators"
-import { renameGraphKeys } from "graphology-utils"
+import { type Attributes } from "graphology-types"
+import { isGraph } from "graphology-utils"
 
 /**
  *
@@ -8,29 +8,83 @@ import { renameGraphKeys } from "graphology-utils"
  *
  */
 export function toUndirectedKeepEdgeNames<TGraph extends Graph>(graph: TGraph) {
-	const tempSymbol = Symbol("temp")
-	const g2 = graph.copy()
-	g2.updateEachEdgeAttributes((...args) => {
-		return {
-			...args[1],
-			[tempSymbol]: args[0],
-		}
-	})
-	const g3 = toUndirected(g2)
-	const changes = g3.reduceEdges(
-		(acc, edge, attr) => {
-			//@ts-expect-error symbols can be used
-			acc[edge] = attr[tempSymbol]
-			return acc
-		},
-		{} as Record<string, string>
-	)
-	const undirectedGraph = renameGraphKeys(g3, {}, changes)
-	undirectedGraph.updateEachEdgeAttributes((_edge, attr) => {
-		//@ts-expect-error symbols can be used
-		delete attr[tempSymbol]
-		return attr
+	return toUndirected(graph) as TGraph
+}
+
+function toUndirected(graph: Graph, options?: any) {
+	if (!isGraph(graph))
+		throw new Error(
+			"graphology-operators/to-undirected: expecting a valid graphology instance."
+		)
+
+	if (typeof options === "function") options = { mergeEdge: options }
+
+	options = options || {}
+
+	const mergeEdge =
+		typeof options.mergeEdge === "function" ? options.mergeEdge : null
+
+	if (graph.type === "undirected") return graph.copy()
+
+	const undirectedGraph = graph.emptyCopy({ type: "undirected" })
+
+	// Adding undirected edges
+	graph.forEachUndirectedEdge(function (edge, attr, source, target) {
+		copyEdge(undirectedGraph, true, edge, source, target, attr)
 	})
 
-	return undirectedGraph as TGraph
+	// TODO: one loop for better performance
+
+	// Merging directed edges
+	graph.forEachDirectedEdge(function (edge, attr, source, target) {
+		if (!graph.multi) {
+			const existingEdge = undirectedGraph.edge(source, target)
+
+			if (existingEdge) {
+				// We need to merge
+				if (mergeEdge)
+					undirectedGraph.replaceEdgeAttributes(
+						existingEdge,
+						mergeEdge(
+							undirectedGraph.getEdgeAttributes(existingEdge),
+							attr
+						)
+					)
+
+				return
+			}
+		}
+		// changed 3rd arg null -> edge
+		copyEdge(undirectedGraph, true, edge, source, target, attr)
+	})
+
+	return undirectedGraph
+}
+
+function copyEdge<EdgeAttributes extends Attributes = Attributes>(
+	graph: Graph,
+	undirected: boolean,
+	key: unknown,
+	source: unknown,
+	target: unknown,
+	attributes?: EdgeAttributes
+): string {
+	attributes = Object.assign({}, attributes)
+
+	if (undirected) {
+		if (key === null || key === undefined)
+			return graph.addUndirectedEdge(source, target, attributes)
+		else
+			return graph.addUndirectedEdgeWithKey(
+				key,
+				source,
+				target,
+				attributes
+			)
+	} else {
+		if (key === null || key === undefined)
+			return graph.addDirectedEdge(source, target, attributes)
+		else
+			return graph.addDirectedEdgeWithKey(key, source, target, attributes)
+	}
 }
