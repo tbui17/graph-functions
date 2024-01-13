09e6c6d6a40d5bae931b2297a435d4d7a89b1ee2
sn <86227911+tbui17@users.noreply.github.com>
2024-01-12 17:02:32 -0700

    update groupedgeentries bidirectional option

--- src/edges/groupEdgeEntries.ts
+++ src/edges/groupEdgeEntries.ts
@@ -3,29 +3,31 @@ import { type EdgeEntry } from "graphology-types"
 import { mapCallbackParametersToEdgeEntry } from "."
 import { type InferGraphEdgeEntry } from ".."
 
+
 /**
- * Groups edge entries of a graph based on a provided function.
- *
- * @param {Object} params - The parameters for the function.
- * @param {TGraph} params.graph - The graph to group edge entries from.
- * @param {string | [string, string]} [params.node] - The node or pair of nodes to consider for grouping. If not provided, all edges in the graph are considered.
- * @param {(edge: InferGraphEdgeEntry<TGraph>) => string} params.fn - The function used to group the edge entries. It should return a string that will be used as the key for the group.
- * @param {Extract<keyof Graph, `reduce${string}Edges`>} [params.edgeReduceStrategy="reduceEdges"] - The strategy to use when reducing edges. This determines which types of edges will be iterated over based on their directionality, with reduceEdges iterating over edges of any direction. It should be a method name of the Graph class that starts with "reduce" and ends with "Edges". Default is "reduceEdges".
- * @param {boolean} [params.ignoreParallelEdges=false] - Whether to ignore parallel edges when grouping. If true, only the first edge between two nodes is considered. Default is false.
- *
- * @returns {Record<string, InferGraphEdgeEntry<TGraph>[]>} - An object where each key is a group key returned by the `fn` function, and each value is an array of edge entries belonging to that group.
- *
- * @example
- * const graph = new Graph();
- * // ...add some edges to the graph...
- * const groups = groupEdgeEntries({
- *   graph,
- *   node: 'node1',
- *   fn: (edge) => edge.attributes.type,
- *   edgeReduceStrategy: 'reduceDirectedEdges',
- *   ignoreParallelEdges: true,
- * });
- * // groups now contains edge entries of 'node1', grouped by their 'type' attribute, considering only directed edges and ignoring parallel edges.
+ * Groups edge entries of a graph based on a specified grouping function.
+ * This function allows for the aggregation of edges, optionally considering
+ * the directionality or bidirectionality of the edges, as well as applying
+ * a custom reduction strategy for edge processing.
+ * 
+ * @param {Object} params - The parameters for the edge grouping function.
+ * @param {TGraph} params.graph - The graph instance from 'graphology' to process.
+ * @param {string | [string, string]} [params.node] - The node or node pair to focus the grouping on.
+ *    If not specified, the grouping is applied across all edges in the graph.
+ * @param {function} params.fn - A function that determines the grouping key for an edge.
+ *    This function should return a string that represents the key for grouping.
+ * @param {Extract<keyof Graph, `reduce${string}Edges`>} [params.edgeReduceStrategy="reduceEdges"] - 
+ *    The edge reduction strategy to use. It's a method name pattern from the Graph class,
+ *    such as `reduceEdges`, `reduceOutEdges`, etc., defining how edges are traversed and reduced.
+ * @param {boolean | "bidirectional"} [params.ignoreParallelEdges=false] - 
+ *    Controls how parallel edges are handled. If set to true, any additional edges of the same direction with matching keys from the key generating function will be ignored.
+ * Setting this to "bidirectional" will perform a similar operation, but ignores directionality.
+ * 
+ * @returns {Record<string, InferGraphEdgeEntry<TGraph>[]>} - A record (object) where each key is a 
+ *    grouping key returned by `fn`, and its value is an array of edges belonging to that group.
+ * 
+ * @template TGraph - A type extending from the Graph class of 'graphology'.
+ * 
  */
 export function groupEdgeEntries<TGraph extends Graph>({
 	graph,
@@ -38,9 +40,10 @@ export function groupEdgeEntries<TGraph extends Graph>({
 	node?: string | [string, string]
 	fn: (edge: InferGraphEdgeEntry<TGraph>) => string
 	edgeReduceStrategy?: Extract<keyof Graph, `reduce${string}Edges`>
-	ignoreParallelEdges?: boolean
+	ignoreParallelEdges?: boolean | "bidirectional"
 }): Record<string, InferGraphEdgeEntry<TGraph>[]> {
 	const [input, map] = resolveDeps(node, fn, ignoreParallelEdges)
+	
 
 	return graph[edgeReduceStrategy](
 		...(input as [string]),
@@ -57,13 +60,10 @@ export function groupEdgeEntries<TGraph extends Graph>({
 function resolveDeps<T extends EdgeEntry>(
 	node: string | [string, string] | undefined,
 	fn: (edge: T) => string,
-	ignoreParallelEdges: boolean
+	ignoreParallelEdges: boolean | "bidirectional"
 ) {
 	const input = resolveInput(node)
-
-	const map = ignoreParallelEdges
-		? new GroupEdgeEntriesCollectionIgnoreParallel(fn)
-		: new GroupEdgeEntriesCollection(fn)
+	const map = mapFactory(fn, ignoreParallelEdges)
 	return [input, map] as const
 }
 
@@ -75,6 +75,22 @@ function resolveInput(node: string | [string, string] | undefined) {
 		return [node[0], node[1]]
 	}
 	return [node]
+}
+
+function mapFactory<T extends EdgeEntry>(
+	fn: (edge: T) => string,
+	ignoreParallelEdges: boolean | "bidirectional"
+) {
+	switch (ignoreParallelEdges) {
+		case true:
+			return new GroupEdgeEntriesCollectionIgnoreParallel(fn)
+		case "bidirectional":
+			const map = new GroupEdgeEntriesCollectionIgnoreParallel(fn)
+			map.createPairKey = createPairKeySorted
+			return map
+		default:
+			return new GroupEdgeEntriesCollection(fn)
+	}
 }
 
 class GroupEdgeEntriesCollection<T> {
@@ -89,6 +105,8 @@ class GroupEdgeEntriesCollection<T> {
 	}
 }
 
+
+
 class GroupEdgeEntriesCollectionIgnoreParallel<
 	T extends EdgeEntry,
 > extends GroupEdgeEntriesCollection<T> {
@@ -97,7 +115,7 @@ class GroupEdgeEntriesCollectionIgnoreParallel<
 	override add(edge: T) {
 		const key = this.keyGetter(edge)
 		const set = this.pairMap[key] ?? new Set()
-		const pair = `${edge.source}${edge.target}`
+		const pair = this.createPairKey(edge)
 		if (set.has(pair)) {
 			return this
 		}
@@ -108,4 +126,12 @@ class GroupEdgeEntriesCollectionIgnoreParallel<
 		this.data[key] = arr
 		return this
 	}
+
+	createPairKey(edge: T) {
+		return `${edge.source}${edge.target}`
+	}
+}
+
+function createPairKeySorted(edge: EdgeEntry) {
+	return [edge.source, edge.target].sort().join("")
 }
